4a9aab099dd891ce01ec2469ffffaf94c8814b04
Felobateer <felobateersamy01@gmail.com>
2023-12-25 10:55:20 +0100

    database integration

--- Cashier/Cashier/Form1.Designer.cs
+++ Cashier/Cashier/Form1.Designer.cs
@@ -28,12 +28,70 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            pictureBox1 = new PictureBox();
+            Inventory = new Label();
+            Checkout = new Label();
+            Cashier = new Label();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new Point(230, 12);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(228, 99);
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // Inventory
+            // 
+            Inventory.AutoSize = true;
+            Inventory.Location = new Point(967, 96);
+            Inventory.Name = "Inventory";
+            Inventory.Size = new Size(57, 15);
+            Inventory.TabIndex = 1;
+            Inventory.Text = "Inventory";
+            Inventory.Click += label1_Click;
+            // 
+            // Checkout
+            // 
+            Checkout.AutoSize = true;
+            Checkout.Location = new Point(1178, 96);
+            Checkout.Name = "Checkout";
+            Checkout.Size = new Size(58, 15);
+            Checkout.TabIndex = 2;
+            Checkout.Text = "Checkout";
+            // 
+            // Cashier
+            // 
+            Cashier.AutoSize = true;
+            Cashier.Location = new Point(1077, 96);
+            Cashier.Name = "Cashier";
+            Cashier.Size = new Size(46, 15);
+            Cashier.TabIndex = 3;
+            Cashier.Text = "Cashier";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1467, 699);
+            Controls.Add(Cashier);
+            Controls.Add(Checkout);
+            Controls.Add(Inventory);
+            Controls.Add(pictureBox1);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private PictureBox pictureBox1;
+        private Label Inventory;
+        private Label Checkout;
+        private Label Cashier;
     }
 }
---
+++ Cashier/Cashier/system/Data.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cashier.system
+{
+    internal class Data
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public int Quantity { get; set; }
+
+        static string myconnstrng = ConfigurationManager.ConnectionStrings[""].ConnectionString;
+
+    }
+}
