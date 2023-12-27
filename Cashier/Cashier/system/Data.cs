1aae8d79b3fe6d729eb4c90a51f39042cb8840b6
Felobateer <felobateersamy01@gmail.com>
2023-12-27 07:39:23 +0100

    update

--- Cashier/Cashier/system/Data.cs
+++ Cashier/Cashier/system/Data.cs
@@ -9,12 +9,25 @@ namespace Cashier.system
 {
     internal class Data
     {
+        private static int lastId = 0;
+        public Data() {
+            Id = GenerateUniqueId();
+        }
+
         public int Id { get; set; }
         public string Name { get; set; }
         public string Category { get; set; }
+        public int Price { get; set; }
         public int Quantity { get; set; }
 
+        // If this is meant to be a connection string, it might be better placed in your configuration file.
         static string myconnstrng = ConfigurationManager.ConnectionStrings[""].ConnectionString;
-
+    
+        private int GenerateUniqueId()
+        {
+            return ++lastId;
+        }
     }
+
+
 }
---
+++ Cashier/Cashier/system/actions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace Cashier.system
+{
+    internal class Actions
+    {
+        public void addItem(Data item)
+        {
+            item.Id = item.generateUniqueId();
+            // Copy other properties
+            item.Name = item.Name;
+            item.Category = item.Category;
+            item.Price = item.Price;
+            item.Quantity = item.Quantity;
+        }
+    }
+}
